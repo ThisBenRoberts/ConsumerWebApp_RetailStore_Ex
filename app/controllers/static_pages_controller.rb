3a9d207e84680499d6eef89436222202753c4977
Ben Roberts <benrobertsmemphis@gmail.com>
2015-10-13 18:36:17 -0600

    Fixed email controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,35 +1,35 @@
 class StaticPagesController < ApplicationController
-  def index
-  end
+        def index
+        end
 
-  def about
-  end
+        def about
+        end
 
-  def contact
-  end
+        def contact
+        end
 
-  def products
-  end
+        def products
+        end
 
-  def faq
-  end
-  
- def landing_page
-    @featured_product = Product.first
-  end
-  
+        def faq
+        end
+        
+       def landing_page
+          @featured_product = Product.first
+          @products = Product.limit(3)
+        end
+        
 
-  def landing_page
-  @products = Product.limit(3)
-  end
-end
 
-def thank_you
-  @name = params[:name]
-  @email = params[:email]
-  @message = params[:message]
-  ActionMailer::Base.mail(:from => @email,
-        :to => 'benrobertsmemphis@gmail.com',
-        :subject => "A new contact form message from #{@name}",
-        :body => @message).deliver
+
+        def thank_you
+          @name = params[:name]
+          @email = params[:email]
+          @message = params[:message]
+          ActionMailer::Base.mail(:from => @email,
+                :to => 'benrobertsmemphis@gmail.com',
+                :subject => "A new contact form message from #{@name}",
+                :body => @message).deliver
+        end
+
 end
